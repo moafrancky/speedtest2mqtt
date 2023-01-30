91d5546dda91ae69e3ff034ba7ddbe4cecc34e81
Ant Seddon <devotee101@gmail.com>
2023-01-30 14:06:49 +0000

    Copy to /tmp

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ CRON=${CRON:-0 0,6,12,18 * * *}
 echo "speedtest2mqtt has been started "
 
 declare | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /tmp/container.env
-cp /home/foo/crontab.yml /var/tmp/
+cp /home/foo/crontab.yml /tmp/
 sed -i "/schedule/c\    schedule: \"${CRON}\"" /tmp/crontab.yml
 
 echo "starting cron (${CRON})"
