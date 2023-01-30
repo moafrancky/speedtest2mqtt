3e7b6454b98381ac185f482260b3f1270f01c5fe
Ant Seddon <devotee101@gmail.com>
2023-01-30 13:59:17 +0000

    /tmp instead of writing to the root filesystem

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,9 @@
 CRON=${CRON:-0 0,6,12,18 * * *}
 echo "speedtest2mqtt has been started "
 
-declare | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /var/tmp/container.env
+declare | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /tmp/container.env
 cp /home/foo/crontab.yml /var/tmp/
-sed -i "/schedule/c\    schedule: \"${CRON}\"" /var/tmp/crontab.yml
+sed -i "/schedule/c\    schedule: \"${CRON}\"" /tmp/crontab.yml
 
 echo "starting cron (${CRON})"
-/yacronenv/bin/yacron -c /var/tmp/crontab.yml
+/yacronenv/bin/yacron -c /tmp/crontab.yml
